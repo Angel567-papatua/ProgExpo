adde1941ee63ef4b51b3180b6b0600e8bfff232b
Andre-Guido <amguido@uamv.edu.ni>
2025-06-04 19:37:18 -0600

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,7 @@
 '''
 Autor: Marlon Emanuel Aguilar.
-Versión: 1.9
-descripciondescripcion: Desarrolle un programa que registre la asistencia de estudiantes en tres secciones distintas de
+Versión: 1.0
+Descripción: Desarrolle un programa que registre la asistencia de estudiantes en tres secciones distintas de
 la UAM, durante cinco días de clases. Cada día se tomará asistencia a seis estudiantes por
 sección. El programa deberá contabilizar y mostrar el total de asistencias registradas por
 sección, así como el total general de la semana. 
